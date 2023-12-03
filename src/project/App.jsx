faf93ab46d7d36f84d638e5dcc29ddf0d183b9c7
connectprakash <pp3662504@gmail.com>
2023-12-02 22:34:47 -0800

    task1 may be work

--- src/project/App.jsx
+++ src/project/App.jsx
@@ -1,7 +1,14 @@
 import React, { useState } from 'react'
 import Header from '../component/Header';
-import Section from '../component/Section';
 import './App.css';
+import Boxone from '../component/Boxone';
+import Boxtwo from '../component/Boxtwo';
+import Boxthree from '../component/Boxthree';
+import Boxfour from '../component/Boxfour';
+import Boxfive from '../component/Boxfive';
+import Boxsix from '../component/Boxsix';
+import Boxseven from '../component/Boxseven';
+import Boxeight from '../component/Boxeight';
 
 function App() {
     const [first, setfirst] = useState(0);
@@ -30,16 +37,28 @@ function App() {
                           <button className="btn btn-outline-dark" type="submit">
                               <i className="bi-cart-fill me-1"></i>
                               Cart
-                                <span className="badge bg-dark text-white ms-1 rounded-pill">{first}</span>
+                                <span className="badge bg-dark text-white ms-1 rounded-pill"> {first}</span>
                           </button>
                       </form>
                   </div>
               </div>
           </nav> 
           <Header />
-         
-            <Section first={setfirst} second={first} />
-             </div>       
+            <section className="py-5">
+                <div className="container px-4 px-lg-5 mt-5">
+                    <div className="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center">     
+                    <Boxone first={setfirst} second={first} />
+                    <Boxtwo first={setfirst} second={first} />
+                    <Boxthree first={setfirst} second={first} />
+                   <Boxfour first={setfirst} second={first} />
+                   <Boxfive first={setfirst} second={first} />
+                   <Boxsix first={setfirst} second={first} />
+                  <Boxseven first={setfirst} second={first} />
+                  <Boxeight first={setfirst} second={first} />
+                    </div>
+                </div>
+            </section>
+            </div>
          
   )
 }
